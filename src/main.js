a94a7a86680272e2ba0d320f6d35b3f208159ed1
Yannis Becker <yannis.becker@tu-ilmenau.de>
2020-05-24 18:01:31 +0200

    add latex math support

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import store from './store';
-import './assets/app.scss';
 import 'bootstrap';
+import './assets/app.scss';
+import 'katex/dist/katex.min.css';
 
 import router from './router';
 import Init from './common/init';
