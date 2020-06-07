0916f7d85f0b18013c5aaf56da045cdfd81588c3
Steve2955 <yanbec2000@gmail.com>
2020-06-07 13:22:32 +0000

    Deploying to gh-pages from  d403329fac64211d7ee822fa147790277ad70188

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/flashmd/precache-manifest.0d9f5250c3f90aa7e84b63703a270bfb.js"
+  "/flashmd/precache-manifest.2309e37e24a29765b8477c9c485ec37e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "flashmd"});
